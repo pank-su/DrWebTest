ae2446efcecb568f24925a28ee8d4f6e9527f07b
pank-su <pank@pank.su>
2025-11-23 13:44:52 +0300

    :recycle: реализация получение хеша из apk файла

--- app/src/main/java/su/pank/drwebtest/data/apps/DefaultAppsRepository.kt
+++ app/src/main/java/su/pank/drwebtest/data/apps/DefaultAppsRepository.kt
@@ -1,16 +1,23 @@
 package su.pank.drwebtest.data.apps
 
 import android.content.Context
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
-import android.os.Build
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import su.pank.drwebtest.data.model.App
+import java.io.File
+import java.io.FileInputStream
 import java.security.MessageDigest
 
-class DefaultAppsRepository(private val context: Context) : AppsRepository {
+class DefaultAppsRepository(
+    context: Context,
+    ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : AppsRepository {
     private val pm = context.packageManager
+
+    // Можно сделать загрузку с помощью Pager
     override val apps: Flow<List<App>> = flow {
         pm.getInstalledApplications(0).map { info ->
             App(
@@ -18,30 +25,26 @@ class DefaultAppsRepository(private val context: Context) : AppsRepository {
                 version = pm.getPackageInfo(info.packageName, 0).versionName
                     ?: "N/A",
                 packageName = info.packageName,
-                hashSum = getAppSignatureHash(info.packageName) ?: "N/A"
+                hashSum = getApkHash(info.sourceDir)
             )
-        }.sortedBy { it.name }.also({ emit(it) })
-    }
-
-    private fun getAppSignatureHash(packageName: String, algorithm: String = "SHA-256"): String? {
-        val packageInfo = pm.getPackageInfo(
-            packageName,
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) PackageManager.GET_SIGNING_CERTIFICATES else PackageManager.GET_SIGNATURES
-        )
-        val signatures = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            packageInfo.signingInfo?.apkContentsSigners ?: emptyArray()
-        } else {
-            packageInfo.signatures ?: emptyArray()
+        }.sortedBy { it.name }.also {
+            emit(it)
         }
+    }.flowOn(ioDispatcher)
 
-        if (signatures.isEmpty()) return null
-
-
-        val signatureBytes = signatures[0].toByteArray()
-
+    private fun getApkHash(sourceDir: String, algorithm: String = "SHA-256"): String {
+        val file = File(sourceDir)
         val digest = MessageDigest.getInstance(algorithm)
-        val hashBytes = digest.digest(signatureBytes)
+        FileInputStream(file).use {
+            val buffer = ByteArray(1024 * 8)
 
-        return hashBytes.toHexString()
+            do {
+                val read = it.read(buffer)
+
+                if (read != -1) digest.update(buffer, 0, read)
+            } while (read != -1)
+        }
+        return digest.digest().toHexString()
     }
+
 }
